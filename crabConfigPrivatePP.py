cf70e9213400044aceb1a8290a668a1c335ed9df
Jing <boundino@gmail.com>
2017-11-07 00:59:18 +0100

    update crab.py

--- crabConfigPrivatePP.py
+++ crabConfigPrivatePP.py
@@ -3,7 +3,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 config.General.transferOutputs = True
-config.General.requestName = 'HLT_pp_20171013_prompt_D0pt0p0_Pthat100_TuneCUETP8M1_5020GeV_pythia8_20171005'
+config.General.requestName = 'HLT_V33_pp_20171106_prompt_D0pt0p0_Pthat100_TuneCUETP8M1_5020GeV_pythia8_20171005'
 config.General.workArea = 'crab_projects_HLT'
 config.JobType.psetName = 'hlt92X.py'
 config.JobType.pluginName = 'Analysis'
@@ -26,3 +26,8 @@ config.Site.storageSite = 'T2_US_MIT'
 config.Site.whitelist = ['T2_US_MIT']
 config.Site.blacklist = ['T2_US_Nebraska','T2_US_Purdue','T2_US_UCSD','T2_US_Wisconsin','T2_FR_GRIF_IRFU','T3_US_UCR','T3_US_Rutgers','T3_BG_UNI_SOFIA','T3_IT_Perugia']
 # config.Site.ignoreGlobalBlacklist = True
+
+# config.Data.inputDataset = '/Pythia8_prompt_D0pt0p0_Pthat50_TuneCUETP8M1_5020GeV/twang-crab_RECO_20171005-a9e0e02b6ce26195d58103316ce7f5b3/USER'
+# config.Data.inputDataset = '/Pythia8_prompt_Dspt0p0_Pthat50_TuneCUETP8M1_5020GeV/twang-crab_RECO_20171005-a9e0e02b6ce26195d58103316ce7f5b3/USER'
+# config.Data.inputDataset = '/Pythia8_Dijet80_pp_CUETP8M1_5020GeV/gsfs-RECO__201711004-b3ceaa6c7c762fab18b245ad1faf35c4/USER'
+# config.Data.inputDataset = '/Pythia8_MinBias_pp_CUETP8M1_5020GeV/gsfs-RECO__201711004-b3ceaa6c7c762fab18b245ad1faf35c4/USER'
