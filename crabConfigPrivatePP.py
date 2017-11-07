ef6ca9c0dcd21762d37eee3f1a8065f4645ff9b2
Jing <boundino@gmail.com>
2017-11-07 01:14:10 +0100

    update crab.py

--- crabConfigPrivatePP.py
+++ crabConfigPrivatePP.py
@@ -27,6 +27,9 @@ config.Site.whitelist = ['T2_US_MIT']
 config.Site.blacklist = ['T2_US_Nebraska','T2_US_Purdue','T2_US_UCSD','T2_US_Wisconsin','T2_FR_GRIF_IRFU','T3_US_UCR','T3_US_Rutgers','T3_BG_UNI_SOFIA','T3_IT_Perugia']
 # config.Site.ignoreGlobalBlacklist = True
 
+config.section_("Debug")
+config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
+
 # config.Data.inputDataset = '/Pythia8_prompt_D0pt0p0_Pthat50_TuneCUETP8M1_5020GeV/twang-crab_RECO_20171005-a9e0e02b6ce26195d58103316ce7f5b3/USER'
 # config.Data.inputDataset = '/Pythia8_prompt_Dspt0p0_Pthat50_TuneCUETP8M1_5020GeV/twang-crab_RECO_20171005-a9e0e02b6ce26195d58103316ce7f5b3/USER'
 # config.Data.inputDataset = '/Pythia8_Dijet80_pp_CUETP8M1_5020GeV/gsfs-RECO__201711004-b3ceaa6c7c762fab18b245ad1faf35c4/USER'
