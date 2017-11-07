3819a813dda3d72a45657b7fde0ec84b1b5f787c
Jing <boundino@gmail.com>
2017-11-07 20:19:57 +0100

    update menu ver

--- rungetconfig.sh
+++ rungetconfig.sh
@@ -12,7 +12,7 @@ DOEVTANALYZER=0
 SAVEHLTTRACKS=0
 ##
 
-HLTCONFIG="/users/wangj/PPRef2017/DmesonHIHighPtRefPP5TeV2017_V3/V27"
+HLTCONFIG="/users/wangj/PPRef2017/DmesonHIHighPtRefPP5TeV2017_V3/V33"
 GLOBTAG="92X_upgrade2017_realistic_v11"
 
 SAMPLE="root://cms-xrd-global.cern.ch//store/user/twang/Pythia8_prompt_D0pt0p0_Pthat20_TuneCUETP8M1_5020GeV/crab_DIGI_20171005/171009_201555/0000/step2_pp_DIGI_L1_DIGI2RAW_HLT_109.root" # prompt D RAW
